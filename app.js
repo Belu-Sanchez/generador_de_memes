08d711ead07414e08e3b9f14bae97bdf568f110a
Maria Sanchez <mariasanchez@MacBook-Pro-de-Maria.local>
2022-06-18 16:15:08 -0300

    style-and-aside-js

--- app.js
+++ app.js
@@ -1,7 +1,52 @@
-const input = document.getElementById('input');
-const renderText = document.getElementById('render-text');
+const textTopTextarea = document.getElementById('text-top-textarea');
+const topText = document.getElementById('top-text');
+const textBottomTextarea = document.getElementById('text-bottom-textarea');
+const bottomText = document.getElementById('bottom-text');
 
-input.addEventListener('input', (event ) => {
-    const textoIngresado = event.target.value;
-    renderText.innerHTML = textoIngresado;
+const textIcon = document.getElementById('text-icon');
+const asideText = document.getElementById('aside-text');
+const btnXtext = document.getElementById('btn-x-text')
+
+const imageIcon = document.getElementById('image-icon');
+const btnXimage = document.getElementById('btn-x-image');
+const asideImage = document.getElementById('aside-image');
+
+// *********************************************
+// modificar el texto top y bottom
+// *********************************************
+
+textTopTextarea.addEventListener('input', (event)=>{
+    const textoIngresado =  event.target.value;
+    topText.innerHTML = textoIngresado
+})
+
+textBottomTextarea.addEventListener('input', (event) =>{
+    const textoIngresado =  event.target.value;
+    bottomText.innerHTML = textoIngresado
+})
+// *********************************************
+// click boton texto y ocultar mostrar aside
+// *********************************************
+
+textIcon.addEventListener('click', ()=>{
+    asideText.classList.remove('oculto')
+    asideImage.classList.add('oculto')
+})
+
+btnXtext.addEventListener('click', ()=>{
+    asideText.classList.add('oculto')
+})
+
+// *********************************************
+// click boton imagen y ocultar mostrar aside
+// *********************************************
+
+
+imageIcon.addEventListener('click', ()=>{
+    asideImage.classList.remove('oculto')
+    asideText.classList.add('oculto')
+})
+
+btnXimage.addEventListener('click', ()=>{
+    asideImage.classList.add('oculto')
 })
