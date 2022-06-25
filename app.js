b9e84c017aaaba7e88774c22f795aa88136d996b
Maria Sanchez <mariasanchez@MacBook-Pro-de-Maria.local>
2022-06-24 23:16:19 -0300

    aside-image-finish

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const bottomBox = document.getElementById('bottom-box');
 
 const transparentBackground = document.getElementById('transparent-background');
 
-const masterBox = document.getElementById('master-box'); 
+const masterBox = document.getElementById('master-box-img'); 
 const inputUrl = document.getElementById('input-url');
 
 const brightnessRange = document.getElementById('brightness-range-input');
@@ -28,23 +28,18 @@ const blurRangeInput = document.getElementById('blur-range-input');
 const grayscaleRangeInput = document.getElementById('grayscale-range-input');
 const sepiaRangeInput = document.getElementById('sepia-range-input');
 const hueRotationRangeInput = document.getElementById('hue-rotation-range-input');
-const sturarionRangeInput = document.getElementById('sturarion-range-input');
+const saturationRangeInput = document.getElementById('sturarion-range-input');
 const invertRangeInput = document.getElementById('invert-range-input');
 
+const resetButton = document.getElementById('reset-button');
 
-// *********************************************
-// modificar el texto top y bottom
-// *********************************************
+const colorFondoInput = document.getElementById('color-fondo-input');
+const colorFondoLabel = document.getElementById('color-fondo-label');
+const blackBox = document.getElementById('black-box');
 
-textTopTextarea.addEventListener('input', (event)=>{
-    const textoIngresado =  event.target.value;
-    topText.innerHTML = textoIngresado
-})
+const typeColorText = document.getElementById('type-color-text');
 
-textBottomTextarea.addEventListener('input', (event) =>{
-    const textoIngresado =  event.target.value;
-    bottomText.innerHTML = textoIngresado
-})
+
 // *********************************************
 // click boton texto y ocultar mostrar aside
 // *********************************************
@@ -72,31 +67,9 @@ btnXimage.addEventListener('click', ()=>{
     asideImage.classList.add('oculto')
 })
 
-// **********************************************************
-// click boton sin texto y ocultar mostrar box-white && text
-// **********************************************************
-
-textTopInput.addEventListener('click', ()=>{
-    topBox.classList.toggle('oculto');
-    // topText.classList.toggle('oculto');
-})
-
-textBottomInput.addEventListener('click', ()=>{
-    bottomBox.classList.toggle('oculto');
-    // bottomText.classList.toggle('oculto');
-})
 
 // **********************************************************
-// click boton fondo transparente y ocultar mostrar box-black 
-// **********************************************************
-
-transparentBackground .addEventListener('click', ()=>{
-    topBox.classList.toggle('oculto');
- })
-
-
-// **********************************************************
-// Mostrar imagen
+// IMAGEN - URL Mostrar imagen
 // **********************************************************
 
 inputUrl.addEventListener('input', (event) => {
@@ -105,7 +78,28 @@ inputUrl.addEventListener('input', (event) => {
 })
 
 // **********************************************************
-// Filtros imagen -
+// IMAGEN - Filtro imagen - INPUT COLOR
+// **********************************************************
+
+colorFondoInput.addEventListener('input', (event) =>{
+    const filterTypeColor = event.target.value;
+    blackBox.style.background = filterTypeColor
+    colorFondoLabel.innerHTML = `${filterTypeColor}`
+})
+
+// **********************************************************
+// IMAGEN - BLEND MODES
+// **********************************************************
+const selectBlendModes = document.getElementById('select-blend-modes');
+
+selectBlendModes.addEventListener('change', (event) =>{
+    const blendModes = event.target.value;
+    masterBox.style.mixBlendMode = blendModes;
+});
+
+
+// **********************************************************
+// IMAGEN - Filtros imagen -
 // **********************************************************
 
 brightnessRange.addEventListener('input', (event) => {
@@ -143,11 +137,10 @@ hueRotationRangeInput.addEventListener('input', (event) => {
     masterBox.style.filter = `hue-rotate(${hueRotationRangeImage}deg)`
 })
 
-sturarionRangeInput.addEventListener('input', (event) => {
-    const  sturarionRangeImage = event.target.value;
-    masterBox.style.filter = `saturate(${sturarionRangeImage}%)`
+saturationRangeInput.addEventListener('input', (event) => {
+    const  saturationRangeImage = event.target.value;
+    masterBox.style.filter = `saturate(${saturationRangeImage}%)`
 })
-
 
 invertRangeInput.addEventListener('input', (event) => {
     const  invertRangeImage = event.target.value;
@@ -155,33 +148,113 @@ invertRangeInput.addEventListener('input', (event) => {
 })
 
 // **********************************************************
-// Filtro imagen - INPUT COLOR
+// IMAGEN - RESET
 // **********************************************************
 
-const typeColor = document.getElementById('type-color');
-const blackBox = document.getElementById('black-box');
-
-typeColor.addEventListener('input', (event) =>{
-    const filterTypeColor = event.target.value;
-    blackBox.style.background = filterTypeColor
+resetButton.addEventListener('click', () => {
+    brightnessRange.value = 1;
+    opacityRangeInput.value = 1;
+    contrastRangeInput.value = 1;
+    blurRangeInput.value = 0.1;
+    grayscaleRangeInput.value = 0;
+    sepiaRangeInput.value = 0;
+    hueRotationRangeInput.value = 0;
+    saturationRangeInput.value = 10;
+    invertRangeInput.value = 0;
+    masterBox.style.filter = 'none'
 })
 
 
+// *********************************************
+// TEXT - modificar el texto top y bottom
+// *********************************************
 
-// **********************************************************
-// Reset Filtros imagen - INCOMPLETO
-// **********************************************************
-
-const imageFilter = document.getElementById('image-filter');
-const resetButton = document.getElementById('reset-button');
-   console.log(document.getElementById('reset-button'))
-
-
-
- resetButton.addEventListener('click', (event)=>{  
-  const resetFilter = event.target.value;
-  alert('hola')
-  brightnessRange.reset(resetFilter)
+textTopTextarea.addEventListener('input', (event)=>{
+    const textoIngresado =  event.target.value;
+    topText.innerHTML = textoIngresado
 })
 
-//document.getElementById('image-filter').reset()
+textBottomTextarea.addEventListener('input', (event) =>{
+    const textoIngresado =  event.target.value;
+    bottomText.innerHTML = textoIngresado
+})
+
+// **********************************************************
+// TEXT - click INPUT sin texto y ocultar / mostrar text/white
+// **********************************************************
+
+textTopInput.addEventListener('input', ()=>{
+    topText.classList.toggle('oculto');
+})
+
+textBottomInput.addEventListener('input', ()=>{
+    bottomText.classList.toggle('oculto');
+})
+transparentBackground.addEventListener('input', ()=>{
+    topText.classList.toggle('oculto');
+    bottomText.classList.toggle('oculto');
+
+
+})
+
+ // **********************************************************
+// TEXT - FUENTE - aligment-style 
+// **********************************************************
+
+const buttonAlignStart = document.getElementById('button-align-left');
+
+buttonAlignStart.addEventListener('click', (event)=>{  
+    const alignLeft = event.target.value;
+    topText.style.textAlign = `left${alignLeft}`
+    bottomText.style.textAlign = `left${alignLeft}`
+
+  })
+  
+const buttonAlignCenter = document.getElementById('button-align-center');
+
+
+buttonAlignCenter.addEventListener('click', (event)=>{  
+    const alignCenter = event.target.value;
+    topText.style.textAlign = `center${alignCenter}`
+    bottomText.style.textAlign = `center${alignCenter}`
+
+  })
+
+
+  const buttonAlignRight = document.getElementById('button-align-right');
+
+
+  buttonAlignRight.addEventListener('click', (event)=>{  
+      const alignRight = event.target.value;
+      topText.style.textAlign = `right${alignRight}`
+      bottomText.style.textAlign = `right${alignRight}`
+  
+    })
+// **********************************************************
+// **********************************************************
+// TEXT - number font = INCOMPLETO
+// **********************************************************
+const  textSizeInput = document.getElementById('text-size-input');
+
+textSizeInput.addEventListener('input', (event) => {
+    const NumberFont = event.target.value;
+    topText.style.fontSize = NumberFont
+    bottomText.style.fontSize = NumberFont
+})
+
+
+//? VER CUANDO CUANDO EL COLOR DE TEXTO O FONDO DEJA DE ANDAR EL BOTON TRANSPARENTE. 
+
+
+
+ // **********************************************************
+// TEXT - Filtro color fondo - INPUT COLOR
+// **********************************************************
+
+const typeColorBox = document.getElementById('type-color-box');
+
+typeColorBox.addEventListener('input', (event) =>{
+    const editColorBox = event.target.value;
+    topBox.style.background = editColorBox 
+    bottomBox.style.background = editColorBox 
+})
