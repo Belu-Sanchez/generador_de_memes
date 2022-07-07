de34feaac34eb6210bed6d0b5ccc358ea26dd131
Maria Sanchez <mariasanchez@MacBook-Pro-de-Maria.local>
2022-07-07 18:57:12 -0300

    btn descarga

--- app.js
+++ app.js
@@ -410,35 +410,6 @@ modoOscuroButton.addEventListener('click', ()=>{
 // DOWLOAD MEME
 // **********************************************************
 
-//     domtoimage.toPng(document.getElementById('black-box'))
-//    .then((dataUrl) => {
- 
-//     aDowloadButton.download = 'my-meme.png'
-//     aDowloadButton.href = dataUrl
-     
-    
-//    })
-
-// $(document).ready(function(){
-
-	
-//     var element = $("#html-content-holder"); // global variable
-//     var getCanvas; // global variable
-     
-//         $("#btn-Preview-Image").on('click', function () {
-//              html2canvas(element, {
-//              onrendered: function (canvas) {
-//                     $("#previewImage").append(canvas);
-//                     getCanvas = canvas;
-//                  }
-//              });
-//         });
-    
-//         $("#btn-Convert-Html2Image").on('click', function () {
-//         var imgageData = getCanvas.toDataURL("image/png");
-//         // Now browser starts downloading it instead of just showing it
-//         var newData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
-//         $("#btn-Convert-Html2Image").attr("download", "your_pic_name.png").attr("href", newData);
-//         });
-    
-//     });
+aDowloadButton.addEventListener('click', () =>
+    domtoimage.toBlob(document.getElementById('black-box')).then((blob) =>
+        saveAs(blob, "mi-meme.png")))
